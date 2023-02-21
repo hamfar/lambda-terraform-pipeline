b1ad7dc0a487c38335a7b17abb61b2391e3de6c7
Hamza Fareed <hamza.fareed@codeo.co.za>
2023-02-21 20:41:05 +0200

    update python version used

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
-  runtime       = "python3.6"
+  runtime       = "python3.9"
 
   ephemeral_storage {
     size = 10240 # Min 512 MB and the Max 10240 MB
